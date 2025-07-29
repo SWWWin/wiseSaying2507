944b1e9deb5b5032dd7e9970957b5c7121e81d98
SWWWin <bgpd1258@naver.com>
2025-07-29 16:12:29 +0900

    feat: 4단계 구현 완료

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("== 명언 앱 ==");
 
+        int wiseSayingCount = 1;
         while(true) {
             System.out.print("명령) ");
             String str = sc.nextLine().trim();
@@ -18,7 +18,8 @@ public class Main {
 
                 System.out.print("작가 : ");
                 String author = sc.nextLine().trim();
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.println(wiseSayingCount + "번 명언이 등록되었습니다.");
+                wiseSayingCount ++;
             }
         }
     }

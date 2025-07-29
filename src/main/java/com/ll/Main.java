397401793780757b1f668e88f4035533be79478a
SWWWin <bgpd1258@naver.com>
2025-07-29 16:07:17 +0900

    feat: 1단계 구현 완료

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,17 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+
+        while(true) {
+            System.out.print("명령) ");
+            String str = sc.nextLine().trim();
+            if(str.equals("종료")) break;
+        }
     }
 }

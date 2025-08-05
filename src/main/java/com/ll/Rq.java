18b674f45a32ce0832aed76a35cea475ee6116a0
SWWWin <bgpd1258@naver.com>
2025-08-05 10:50:13 +0900

    feat: red case 생성

---
+++ src/main/java/com/ll/Rq.java
@@ -0,0 +1,49 @@
+package com.ll;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Rq {
+    private final String cmd;
+    private final Map<String, String> params;
+
+
+    public Rq(String cmd) {
+        this.cmd = cmd;
+
+     //   params = new HashMap<>();
+        //"등록?이름=홍길동&고향=남원" -> "등록", "이름=홍길동&고향=남원"
+
+
+//        //이름=홍길동&고향=남원
+//        String[] queryStringBits = queryString.split("&");
+//
+//        // [이름=홍길동, 고향=남원]
+//        for (String paramStr  : queryStringBits) {
+//            // "이름=홍길동" -> "이름", "홍길동"
+//            // "고향=남원" -> "고향", "남원"
+//            String[] paramBits = paramStr.split("=", 2);
+//            String paramName = paramBits[0];
+//            String paramValue = paramBits[1];
+//
+//            params.put(paramName, paramValue);
+//        }
+
+        String queryString = cmd.split("\\?", 2)[1];
+
+        params = Arrays.stream(queryString.split("&"))
+                .map(e -> e.split("=", 2))
+                .collect(Collectors.toMap(e -> e[0], e-> e[1]));
+
+    }
+
+    public String getActionName() {
+        String[] cmdBits = cmd.split("\\?");
+        return cmdBits[0];
+    }
+
+    public String getParam(String name, String defaultValue) {
+        return params.getOrDefault(name,defaultValue);
+    }
+}
